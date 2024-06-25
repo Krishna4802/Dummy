be2791467fb3e36311564dce07b47cc73e5ed422
Krishna4802 <139359113+Krishna4802@users.noreply.github.com>
2024-06-25 10:14:42 +0530

    Update chunk.sql

--- chunk.sql
+++ chunk.sql
@@ -46,11 +46,21 @@ BEGIN
         FROM 
             EntityReferences er
         CROSS APPLY 
-            dbo.get_direct_references(
-                CASE 
-                    WHEN dr.referenced_entity LIKE 'input.mv_%' THEN OBJECT_ID(REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_'))
-                    ELSE dr.referenced_entity_id
-                END
+            (
+                SELECT 
+                    referenced_entity = CAST(
+                        CASE 
+                            WHEN dr.referenced_entity LIKE 'input.mv_%' THEN REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_')
+                            ELSE dr.referenced_entity
+                        END AS NVARCHAR(256)
+                    ),
+                    referenced_entity_id = 
+                        CASE 
+                            WHEN dr.referenced_entity LIKE 'input.mv_%' THEN OBJECT_ID(REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_'))
+                            ELSE dr.referenced_entity_id
+                        END
+                FROM 
+                    dbo.get_direct_references(er.referenced_entity_id) dr
             ) dr
         WHERE 
             CHARINDEX(
@@ -63,24 +73,14 @@ BEGIN
     )
     SELECT 
         base_entity,
-        MAX(CASE WHEN level = 1 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_1,
-        MAX(CASE WHEN level = 2 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_2,
-        MAX(CASE WHEN level = 3 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_3,
-        MAX(CASE WHEN level = 4 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_4,
-        MAX(CASE WHEN level = 5 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_5,
-        MAX(CASE WHEN level = 6 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_6,
-        MAX(CASE WHEN level = 7 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_7,
-        MAX(CASE WHEN level = 8 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_8,
-        MAX(CASE WHEN level = 9 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_9,
-        MAX(CASE WHEN level = 10 THEN referenced_entity ELSE NULL END) AS referenced_entity_level_10,
+        referenced_entity,
+        level,
         path
     FROM 
         EntityReferences
     WHERE 
         referenced_entity != base_entity
-    GROUP BY 
-        base_entity, path
     ORDER BY 
-        base_entity, path
+        base_entity, level, referenced_entity
     OPTION (MAXRECURSION 0); -- Allow unlimited recursion
 END;
