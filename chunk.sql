b25b6237a093d32f5fda5de526f6de11312ed999
Krishna4802 <139359113+Krishna4802@users.noreply.github.com>
2024-06-25 11:15:43 +0530

    Update chunk.sql

--- chunk.sql
+++ chunk.sql
@@ -1,59 +1,7 @@
-CREATE PROCEDURE dbo.get_all_references
-(
-    @object_name NVARCHAR(256)
-)
-AS
-BEGIN
-    DECLARE @object_id INT;
-    SELECT @object_id = OBJECT_ID(@object_name);
-
-    IF @object_id IS NULL
+    -- Adjust @object_name if it starts with 'input.mv_'
+    IF LEFT(@object_name, 8) = 'input.mv_'
     BEGIN
-        RAISERROR('Invalid object name: %s', 16, 1, @object_name);
-        RETURN;
+        SET @object_name = 'input.vw_' + SUBSTRING(@object_name, 9, LEN(@object_name));
     END
 
-    ;WITH EntityReferences AS
-    (
-        -- Anchor member: start with the given stored procedure
-        SELECT 
-            base_entity = @object_name,
-            referenced_entity = @object_name,
-            referenced_entity_id = @object_id,
-            level = 0,
-            path = CAST(@object_name AS NVARCHAR(MAX))
-        UNION ALL
-        -- Recursive member: get references for each referenced entity
-        SELECT 
-            er.base_entity,
-            CASE 
-                WHEN dr.referenced_entity LIKE 'input.mv_%' THEN REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_')
-                ELSE dr.referenced_entity
-            END,
-            dr.referenced_entity_id,
-            er.level + 1,
-            path = CAST(er.path + ' -> ' + 
-                        CASE 
-                            WHEN dr.referenced_entity LIKE 'input.mv_%' THEN REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_')
-                            ELSE dr.referenced_entity
-                        END AS NVARCHAR(MAX))
-        FROM 
-            EntityReferences er
-        CROSS APPLY 
-            dbo.get_direct_references(er.referenced_entity_id) dr
-        WHERE 
-            CHARINDEX(dr.referenced_entity, er.path) = 0 -- Avoid circular references
-    )
-    SELECT
-        base_entity,
-        referenced_entity,
-        level,
-        path
-    FROM 
-        EntityReferences
-    WHERE 
-        referenced_entity != base_entity
-    ORDER BY 
-        base_entity, level, referenced_entity
-    OPTION (MAXRECURSION 0); -- Allow unlimited recursion
-END;
+    SELECT @object_id = OBJECT_ID(@object_name);
