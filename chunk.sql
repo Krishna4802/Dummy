031bd4d843526f85e8ff2af7d51acd95d6a791c0
Krishna4802 <139359113+Krishna4802@users.noreply.github.com>
2024-06-25 11:01:31 +0530

    Update chunk.sql

--- chunk.sql
+++ chunk.sql
@@ -1,4 +1,4 @@
-CREATE OR ALTER PROCEDURE dbo.get_all_references
+CREATE PROCEDURE dbo.get_all_references
 (
     @object_name NVARCHAR(256)
 )
@@ -14,46 +14,46 @@ BEGIN
     END
 
     ;WITH EntityReferences AS
-(
-    -- Anchor member: start with the given stored procedure
-    SELECT 
-        base_entity = @object_name,
-        referenced_entity = @object_name,
-        referenced_entity_id = @object_id,
-        level = 0,
-        path = CAST(@object_name AS NVARCHAR(MAX))
-    UNION ALL
-    -- Recursive member: get references for each referenced entity
-    SELECT 
-        er.base_entity,
-        CASE 
-            WHEN dr.referenced_entity LIKE 'input.mv_%' THEN 'input.vw_' + SUBSTRING(dr.referenced_entity, LEN('input.mv_') + 1, LEN(dr.referenced_entity))
-            ELSE dr.referenced_entity
-        END,
-        dr.referenced_entity_id,
-        er.level + 1,
-        path = TRY_CAST(er.path + '->' + 
-                    CASE 
-                        WHEN dr.referenced_entity LIKE 'input.mv_%' THEN 'input.vw_' + SUBSTRING(dr.referenced_entity, LEN('input.mv_') + 1, LEN(dr.referenced_entity))
-                        ELSE dr.referenced_entity
-                    END AS NVARCHAR(MAX))
+    (
+        -- Anchor member: start with the given stored procedure
+        SELECT 
+            base_entity = @object_name,
+            referenced_entity = @object_name,
+            referenced_entity_id = @object_id,
+            level = 0,
+            path = CAST(@object_name AS NVARCHAR(MAX))
+        UNION ALL
+        -- Recursive member: get references for each referenced entity
+        SELECT 
+            er.base_entity,
+            CASE 
+                WHEN dr.referenced_entity LIKE 'input.mv_%' THEN REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_')
+                ELSE dr.referenced_entity
+            END,
+            dr.referenced_entity_id,
+            er.level + 1,
+            path = CAST(er.path + ' -> ' + 
+                        CASE 
+                            WHEN dr.referenced_entity LIKE 'input.mv_%' THEN REPLACE(dr.referenced_entity, 'input.mv_', 'input.vw_')
+                            ELSE dr.referenced_entity
+                        END AS NVARCHAR(MAX))
+        FROM 
+            EntityReferences er
+        CROSS APPLY 
+            dbo.get_direct_references(er.referenced_entity_id) dr
+        WHERE 
+            CHARINDEX(dr.referenced_entity, er.path) = 0 -- Avoid circular references
+    )
+    SELECT
+        base_entity,
+        referenced_entity,
+        level,
+        path
     FROM 
-        EntityReferences er
-    CROSS APPLY 
-        dbo.get_direct_references(er.referenced_entity_id) dr
+        EntityReferences
     WHERE 
-        CHARINDEX(dr.referenced_entity, er.path) = 0 -- Avoid circular references
-)
-SELECT
-    base_entity,
-    referenced_entity,
-    level,
-    path
-FROM 
-    EntityReferences
-WHERE 
-    referenced_entity != base_entity
-ORDER BY 
-    base_entity, level, referenced_entity
-OPTION (MAXRECURSION 0); 
+        referenced_entity != base_entity
+    ORDER BY 
+        base_entity, level, referenced_entity
+    OPTION (MAXRECURSION 0); -- Allow unlimited recursion
 END;
